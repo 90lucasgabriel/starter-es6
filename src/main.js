f382cd62f95f4ec465b2d9095a8593a0885a5c97
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-03 16:08:19 -0300

    - New: api request;

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import api from './api';
+
 class App {
   constructor() {
     this.repositories = []; // lista de repositórios
     this.formElement = document.getElementById('repo-form'); // elemento do formulário
+    this.inputElement = document.querySelector('input[name=repository]'); // elemento de campo de texto
     this.listElement = document.getElementById('repo-list'); // elemento da lista
     this.registerHandlers(); // registrar as ações do usuário
   }
@@ -16,41 +19,60 @@ class App {
   /**
    * Adiciona repositórios
    */
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault(); // previne o recarregamento da página
+
+    const repositoryInput = this.inputElement.value; // recebe o valor digitado
+    if (repositoryInput.length === 0) {
+      // verifica se o valor é vazio
+      return; // interrompe a execucao da função
+    }
+
+    const response = await api.get(`/repos/${repositoryInput}`); // fazendo a requisicao a api
+    const {
+      name,
+      description,
+      html_url,
+      owner: { avatar_url },
+    } = response.data; // desestruturando a responsta
+
     this.repositories.push({
-      name: 'Lucas Gabriel',
-      description: 'descricao do repositorio descricao do repositorio descricao do repositorio descricao do repositorio',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/9625765?s=400&u=f123476bf3a0547ffc2dfacecbf65ea0a5fc6ebd&v=4',
-      html_url: 'https://github.com/90lucasgabriel',
+      name,
+      description,
+      avatar_url,
+      html_url,
     }); // adicionando à lista com objeto desestruturado
 
+    this.inputElement.value = '';
     this.render();
   }
 
   render() {
     this.listElement.innerHTML = ''; // limpa a lista
 
-    this.repositories.forEach(repository => {
+    this.repositories.forEach((repository) => {
       let imgElement = document.createElement('img'); // cria um elemento de imagem
       imgElement.setAttribute('src', repository.avatar_url); // acrescenta a origem da imagem como atributo
 
       let titleElement = document.createElement('strong'); // cria um elemento de texto
-      titleElement.appendChild(document.createTextNode(repository.name)) // agrega o nome como conteúdo
+      titleElement.appendChild(document.createTextNode(repository.name)); // agrega o nome como conteúdo
 
       let descriptionElement = document.createElement('p'); // cria um elemento de parágrafo
-      descriptionElement.appendChild(document.createTextNode(repository.description)); // agrega a descrição como conteúdo
+      descriptionElement.appendChild(
+        document.createTextNode(repository.description)
+      ); // agrega a descrição como conteúdo
 
       let linkElement = document.createElement('a'); // cria um elemento de link
       linkElement.setAttribute('target', '_blank'); // adiciona o atributo para abrir em outra aba
-      linkElement.appendChild(document.createTextNode('Acessar')) // agrega o texto como conteúdo
+      linkElement.setAttribute('href', repository.html_url); // adiciona o link como atributo
+      linkElement.appendChild(document.createTextNode('Acessar')); // agrega o texto como conteúdo
 
       let listItemElement = document.createElement('li'); // cria um elemento de lista
       listItemElement.appendChild(imgElement); // agrega a imagem ao item da lista
       listItemElement.appendChild(titleElement); // agrega o título ao item da lista
       listItemElement.appendChild(descriptionElement); // agrega a descricao ao item da lista
       listItemElement.appendChild(linkElement); // agrega o link ao item da lista
-      
+
       this.listElement.appendChild(listItemElement);
     });
   }
